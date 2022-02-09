98b3c72e2b50f29e8e0471ca7e18de9e4f936f2b
aliu8606 <adotliu34@gmail.com>
2022-02-09 10:58:38 -0500

    Initial commit

---
+++ src/ArrayListAlgorithms.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class ArrayListAlgorithms
+{
+    public static boolean containsTarget(ArrayList<String> stringList, String target)
+    { /* implement this method! */ }
